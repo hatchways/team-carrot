67b782075ceac453635b44f66056683ea0b2eea4
Manpreet Kaur <manpreetk0294@@gmail.com>
2019-09-26 01:52:29 -0400

    adding amazon scraper

--- server/app.js
+++ server/app.js
@@ -10,6 +10,8 @@ import pingRouter from "./routes/ping";
 import userRouter from "./routes/user";
 import itemRouter from "./routes/item";
 
+import scraper from "./scraper/scraper";
+
 var app = express();
 connectDB();
 
@@ -39,5 +41,8 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.json({ error: err });
 });
-
+// scraper.getProductContent('https://www.amazon.ca/dp/B072KLMKTK/ref=sspa_dk_detail_4?psc=1&pd_rd_i=B072KLMKTK&pd_rd_w=fTEjS&pf_rd_p=4b7c8c1c-293f-4b1e-a49a-8787dff31bcb&pd_rd_wg=YEBIB&pf_rd_r=CZETKVE6SGXQKHJ8SNN7&pd_rd_r=d3a44b22-2a2b-4911-b827-daf3bc1c690b&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzT1hIVUJJR0RGV1REJmVuY3J5cHRlZElkPUEwODU3NDk0MVNDM0MxTlM4MjY1MiZlbmNyeXB0ZWRBZElkPUEwMzc0NzAxWjI0VFlFRE5SSUlBJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==')
+//     .then((product) => {
+//         console.log(product)
+//     });
 module.exports = app;
---
+++ server/scraper/scraper.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+
+const getProductContent = (siteUrl) => {
+    let promise = new Promise(async(resolve, reject) => {
+        const browser = await puppeteer.launch({
+            headless: true
+        });
+        const page = await browser.newPage();
+        await page.goto(siteUrl);
+        const html = await page.content();
+
+        const productTitle = cheerio('#productTitle', html).text().trim();
+        let productPrice;
+        if (await page.$('#priceblock_dealprice') !== null) {
+            productPrice = cheerio('#priceblock_dealprice', html).text();
+        } else if (await page.$('#priceblock_ourprice') !== null) {
+            productPrice = cheerio('#priceblock_ourprice', html).text();
+        } else {
+            console.log("cannot exists");
+        }
+
+        const pictureUrl = await page.evaluate('document.querySelector("#imgTagWrapperId img").getAttribute("src")');
+        await browser.close();
+        resolve({
+            title: productTitle,
+            price: productPrice,
+            pictureUrl: pictureUrl
+        });
+    });
+    return promise;
+}
+
+
+module.exports = {
+    getProductContent: getProductContent
+};
