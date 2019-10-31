e5057d81f075974f2766b406e8daac2d53b2512e
Walihossain <w.hossain@queensu.ca>
2019-10-31 11:52:46 -0400

    Dropdown done

--- client/src/pages/Components/Layouts/DashHeader.js
+++ client/src/pages/Components/Layouts/DashHeader.js
@@ -35,7 +35,7 @@ class DashHeader extends Component {
   }
 
   signOut() {
-    // console.log(this.props.SignOutAction);
+    console.log("signOutisclicked");
     this.props.SignOutAction();
   }
 
